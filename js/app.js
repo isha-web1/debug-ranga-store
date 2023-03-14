6460f73038fcf857a444ccebee61a2a36749d7a5
isha-web1 <isha.web1@gmail.com>
2023-03-14 09:21:11 +0600

    price error solved

--- js/app.js
+++ js/app.js
@@ -74,9 +74,9 @@ const getInputValue = (id) => {
 // main price update function
 const updatePrice = (id, value) => {
    const convertedOldPrice = getInputValue(id);
-   const convertPrice = parseInt(value);
+   const convertPrice = parseFloat(value);
    const total = convertedOldPrice + convertPrice;
-   document.getElementById(id).innerText = Math.round(total);
+   document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
